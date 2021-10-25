52d48e3ad9626735cd9cbbcb31714572bf3fb939
jw209 <jw209@uakron.edu>
2021-10-25 17:44:56 -0400

    Another initial commit

--- server.js
+++ server.js
@@ -28,8 +28,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan('tiny'));
 app.use('/api', routes);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
 }
 
 // start listening
