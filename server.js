8d07300b4f8562d0fff125bc89822da9eec75117
jw209 <jw209@uakron.edu>
2021-10-25 17:22:29 -0400

    Another initial commit

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(morgan('tiny'));
 app.use('/api', routes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('../client/build'));
 }
 
 // start listening
