837450d4db67ceb1e96718f8073817eb25ec8924
jw209 <jw209@uakron.edu>
2021-11-18 21:28:39 -0500

    Reformat register and login routes

--- routes/api.js
+++ routes/api.js
@@ -71,85 +71,71 @@ router.post('/saveGame', (req, res) => {
     })
 })
 
-// @route POST api/users/register
-// @desc Register user
-// @access Public
 router.post("/register", (req, res) => {
-    // Form validation
   const { errors, isValid } = validateRegisterInput(req.body);
-  // Check validation
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   Account.findOne({ email: req.body.email }).then(account => {
-      if (account) {
-        return res.status(400).json({ email: "Email already exists" });
-      } else {
-        const newAccount = new Account({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password
+    if (account) {
+      return res.status(400).json({ email: "Email taken" });
+    } else {
+      const newAccount = new Account({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newAccount.password, salt, (err, hash) => {
+          if (err) throw err;
+          newAccount.password = hash;
+          newAccount
+            .save()
+            .then(account => res.json(account))
+            .catch(err => console.log(err));
         });
-  // Hash password before saving in database
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newAccount.password, salt, (err, hash) => {
-            if (err) throw err;
-            newAccount.password = hash;
-            newAccount
-              .save()
-              .then(account => res.json(account))
-              .catch(err => console.log(err));
-          });
-        });
-      }
-    });
+      });
+    }
+  });
 });
 
-// @route POST api/users/login
-// @desc Login user and return JWT token
-// @access Public
 router.post("/login", (req, res) => {
-    // Form validation
   const { errors, isValid } = validateLoginInput(req.body);
-  // Check validation
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   const email = req.body.email;
-    const password = req.body.password;
-  // Find account by email
-    Account.findOne({ email }).then(account => {
-      // Check if account exists
-      if (!account) {
-        return res.status(404).json({ emailnotfound: "Email not found" });
-      }
-  // Check password
-      bcrypt.compare(password, account.password).then(isMatch => {
-        if (isMatch) {
-          // account matched
-          // Create JWT Payload
-          const payload = {
-            id: account.id,
-            name: account.name
-          };
-  // Sign token
-          jwt.sign(
-            payload,
-            keys.secretOrKey,
-            {
-              expiresIn: 31556926 // 1 year in seconds
-            },
-            (err, token) => {
-              res.json({
-                success: true,
-                token: "Bearer " + token
-              });
-            }
-          );
+  const password = req.body.password;
+
+  Account.findOne({ email }).then(account => {
+    if (!account) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
+    bcrypt.compare(password, account.password).then(isMatch => {
+      if (isMatch) {
+        const payload = {
+          id: account.id,
+          name: account.name
+        };
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 31556926 // 1 year
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
         } else {
           return res
             .status(400)
-            .json({ passwordincorrect: "Password incorrect" });
+            .json({ passwordincorrect: "Invalid password" });
         }
       });
     });
